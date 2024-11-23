69c285ee23b768cb2a39e054fae9a3420de07ac3
Denis <dddenisss35@gmail.com>
2024-11-23 22:39:18 +0200

    NodeBase2

--- TreeMachine.Pro/System.TreeMachine/NodeBase.cs
+++ TreeMachine.Pro/System.TreeMachine/NodeBase.cs
@@ -153,7 +153,7 @@ public abstract class NodeBase<TThis> where TThis : NodeBase<TThis> {
         OnAfterDeactivateInternal( argument );
     }
 
-    // Activate
+    // OnActivate
     private protected virtual void OnBeforeActivateInternal(object? argument) {
         OnBeforeActivateEvent?.Invoke( argument );
         OnBeforeActivate( argument );
@@ -232,54 +232,5 @@ public abstract class NodeBase<TThis> where TThis : NodeBase<TThis> {
     protected virtual void Sort(List<TThis> children) {
         //children.Sort( (a, b) => Comparer<int>.Default.Compare( GetOrderOf( a ), GetOrderOf( b ) ) );
     }
-
-}
-public abstract class NodeBase2<TThis> : NodeBase<TThis> where TThis : NodeBase2<TThis> {
-
-    // OnDescendantActivate
-    public event Action<TThis, object?>? OnBeforeDescendantActivateEvent;
-    public event Action<TThis, object?>? OnAfterDescendantActivateEvent;
-    public event Action<TThis, object?>? OnBeforeDescendantDeactivateEvent;
-    public event Action<TThis, object?>? OnAfterDescendantDeactivateEvent;
-
-    // Constructor
-    public NodeBase2() {
-    }
-
-    // Activate
-    private protected sealed override void OnBeforeActivateInternal(object? argument) {
-        foreach (var ancestor in Ancestors.Reverse()) {
-            ancestor.OnBeforeDescendantActivateEvent?.Invoke( (TThis) this, argument );
-            ancestor.OnBeforeDescendantActivate( (TThis) this, argument );
-        }
-        base.OnBeforeActivateInternal( argument );
-    }
-    private protected sealed override void OnAfterActivateInternal(object? argument) {
-        base.OnAfterActivateInternal( argument );
-        foreach (var ancestor in Ancestors) {
-            ancestor.OnAfterDescendantActivate( (TThis) this, argument );
-            ancestor.OnAfterDescendantActivateEvent?.Invoke( (TThis) this, argument );
-        }
-    }
-    private protected sealed override void OnBeforeDeactivateInternal(object? argument) {
-        foreach (var ancestor in Ancestors.Reverse()) {
-            ancestor.OnBeforeDescendantDeactivateEvent?.Invoke( (TThis) this, argument );
-            ancestor.OnBeforeDescendantDeactivate( (TThis) this, argument );
-        }
-        base.OnBeforeDeactivateInternal( argument );
-    }
-    private protected sealed override void OnAfterDeactivateInternal(object? argument) {
-        base.OnAfterDeactivateInternal( argument );
-        foreach (var ancestor in Ancestors) {
-            ancestor.OnAfterDescendantDeactivate( (TThis) this, argument );
-            ancestor.OnAfterDescendantDeactivateEvent?.Invoke( (TThis) this, argument );
-        }
-    }
-
-    // OnDescendantActivate
-    protected abstract void OnBeforeDescendantActivate(TThis descendant, object? argument);
-    protected abstract void OnAfterDescendantActivate(TThis descendant, object? argument);
-    protected abstract void OnBeforeDescendantDeactivate(TThis descendant, object? argument);
-    protected abstract void OnAfterDescendantDeactivate(TThis descendant, object? argument);
 
 }
