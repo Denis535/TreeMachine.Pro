090b462ef24272992a05e0bf54eb2d9da41b151d
Denis535 <dddenisss35@gmail.com>
2025-03-06 13:45:39 +0200

    Refactoring

---
+++ TreeMachine.Pro.Tests/System.TreeMachine/Node.cs
@@ -0,0 +1,71 @@
+﻿namespace System.TreeMachine {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Text;
+
+    internal class Node : NodeBase5<Node> {
+
+        //public bool IsDisposed { get; private set; }
+
+        public Node() {
+        }
+        //public virtual void Dispose() {
+        //    System.Assert.Operation.Message( $"Node {this} must be non-disposed" ).Valid( !IsDisposed );
+        //    System.Assert.Operation.Message( $"Node {this} must be inactive" ).Valid( Activity == Activity_.Inactive );
+        //    System.Assert.Operation.Message( $"Node {this} must have no tree" ).Valid( Tree == null );
+        //    foreach (var child in Children) {
+        //        child.Dispose();
+        //    }
+        //    IsDisposed = true;
+        //}
+
+        // OnAttach
+        protected override void OnAttach(object? argument) {
+        }
+        protected override void OnDetach(object? argument) {
+        }
+
+        // OnDescendantAttach
+        protected override void OnBeforeDescendantAttach(Node descendant, object? argument) {
+        }
+        protected override void OnAfterDescendantAttach(Node descendant, object? argument) {
+        }
+        protected override void OnBeforeDescendantDetach(Node descendant, object? argument) {
+        }
+        protected override void OnAfterDescendantDetach(Node descendant, object? argument) {
+        }
+
+        // OnActivate
+        protected override void OnActivate(object? argument) {
+            Trace.WriteLine( "OnActivate: " + GetType().Name );
+        }
+        protected override void OnDeactivate(object? argument) {
+            Trace.WriteLine( "OnDeactivate: " + GetType().Name );
+        }
+
+        // OnDescendantActivate
+        protected override void OnBeforeDescendantActivate(Node descendant, object? argument) {
+        }
+        protected override void OnAfterDescendantActivate(Node descendant, object? argument) {
+        }
+        protected override void OnBeforeDescendantDeactivate(Node descendant, object? argument) {
+        }
+        protected override void OnAfterDescendantDeactivate(Node descendant, object? argument) {
+        }
+
+        // AddChild
+        protected override void AddChild(Node child, object? argument) {
+            base.AddChild( child, argument );
+        }
+        protected override void RemoveChild(Node child, object? argument, Action<Node>? callback) {
+            base.RemoveChild( child, argument, callback );
+        }
+
+        // Sort
+        protected override void Sort(List<Node> children) {
+            base.Sort( children );
+        }
+
+    }
+}
--- TreeMachine.Pro.Tests/System.TreeMachine/Tree.cs
+++ TreeMachine.Pro.Tests/System.TreeMachine/Tree.cs
@@ -2,7 +2,6 @@
     using System;
     using System.Collections.Generic;
     using System.Text;
-    using NUnit.Framework;
 
     internal class Tree<T> : ITree<Node> {
 
@@ -23,70 +22,6 @@
         }
         public void RemoveRoot(Node root, object? argument, Action<Node>? callback) {
             ITree<Node>.RemoveRoot( this, root, argument, callback );
-        }
-
-    }
-    internal abstract class Node : NodeBase4<Node> {
-
-        //public bool IsDisposed { get; private set; }
-
-        public Node() {
-        }
-        //public virtual void Dispose() {
-        //    System.Assert.Operation.Message( $"Node {this} must be non-disposed" ).Valid( !IsDisposed );
-        //    System.Assert.Operation.Message( $"Node {this} must be inactive" ).Valid( Activity == Activity_.Inactive );
-        //    System.Assert.Operation.Message( $"Node {this} must have no tree" ).Valid( Tree == null );
-        //    foreach (var child in Children) {
-        //        child.Dispose();
-        //    }
-        //    IsDisposed = true;
-        //}
-
-        // OnAttach
-        protected override void OnAttach(object? argument) {
-        }
-        protected override void OnDetach(object? argument) {
-        }
-
-        // OnDescendantAttach
-        protected override void OnBeforeDescendantAttach(Node descendant, object? argument) {
-        }
-        protected override void OnAfterDescendantAttach(Node descendant, object? argument) {
-        }
-        protected override void OnBeforeDescendantDetach(Node descendant, object? argument) {
-        }
-        protected override void OnAfterDescendantDetach(Node descendant, object? argument) {
-        }
-
-        // OnActivate
-        protected override void OnActivate(object? argument) {
-            TestContext.Out.WriteLine( "OnActivate: " + GetType().Name );
-        }
-        protected override void OnDeactivate(object? argument) {
-            TestContext.Out.WriteLine( "OnDeactivate: " + GetType().Name );
-        }
-
-        // OnDescendantActivate
-        protected override void OnBeforeDescendantActivate(Node descendant, object? argument) {
-        }
-        protected override void OnAfterDescendantActivate(Node descendant, object? argument) {
-        }
-        protected override void OnBeforeDescendantDeactivate(Node descendant, object? argument) {
-        }
-        protected override void OnAfterDescendantDeactivate(Node descendant, object? argument) {
-        }
-
-        // AddChild
-        protected override void AddChild(Node child, object? argument) {
-            base.AddChild( child, argument );
-        }
-        protected override void RemoveChild(Node child, object? argument, Action<Node>? callback) {
-            base.RemoveChild( child, argument, callback );
-        }
-
-        // Sort
-        protected override void Sort(List<Node> children) {
-            base.Sort( children );
         }
 
     }
