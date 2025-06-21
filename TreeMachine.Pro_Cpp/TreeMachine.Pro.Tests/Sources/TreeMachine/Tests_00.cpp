ad7261c176b091eaca1455c26ab10ac40feee736
Denis <dddenisss35@gmail.com>
2025-06-21 19:30:52 +0300

    Cpp

--- TreeMachine.Pro_Cpp/TreeMachine.Pro.Tests/Sources/TreeMachine/Tests_00.cpp
+++ TreeMachine.Pro_Cpp/TreeMachine.Pro.Tests/Sources/TreeMachine/Tests_00.cpp
@@ -4,7 +4,7 @@
 namespace TreeMachine {
     using namespace std;
 
-    class Node final : public NodeBase {
+    class Node : public NodeBase {
 
         public:
         explicit Node() = default;
@@ -45,27 +45,40 @@ namespace TreeMachine {
         // }
 
         public:
-        void AddChild(Node *const child, const any argument) {
-            NodeBase::AddChild(child, argument);
-        }
-        void RemoveChild(Node *const child, const any argument, const function<void(NodeBase *const, const any)> callback) {
-            NodeBase::RemoveChild(child, argument, callback);
-        }
-        bool RemoveChild(const function<bool(NodeBase *const)> predicate, const any argument, const function<void(NodeBase *const, const any)> callback) { // NOLINT
-            return NodeBase::RemoveChild(predicate, argument, callback);
-        }
-        int32_t RemoveChildren(const function<bool(NodeBase *const)> predicate, const any argument, const function<void(NodeBase *const, const any)> callback) { // NOLINT
-            return NodeBase::RemoveChildren(predicate, argument, callback);
-        }
-        void RemoveSelf(const any argument, const function<void(NodeBase *const, const any)> callback) { // NOLINT
-            NodeBase::RemoveSelf(argument, callback);
-        }
+        using NodeBase::AddChild;
+        using NodeBase::RemoveChild;
+        using NodeBase::RemoveChildren;
+        using NodeBase::RemoveSelf;
+        // void AddChild(Node *const child, const any argument) {
+        //     NodeBase::AddChild(child, argument);
+        // }
+        // void RemoveChild(Node *const child, const any argument, const function<void(NodeBase *const, const any)> callback) {
+        //     NodeBase::RemoveChild(child, argument, callback);
+        // }
+        // bool RemoveChild(const function<bool(NodeBase *const)> predicate, const any argument, const function<void(NodeBase *const, const any)> callback) { // NOLINT
+        //     return NodeBase::RemoveChild(predicate, argument, callback);
+        // }
+        // int32_t RemoveChildren(const function<bool(NodeBase *const)> predicate, const any argument, const function<void(NodeBase *const, const any)> callback) { // NOLINT
+        //     return NodeBase::RemoveChildren(predicate, argument, callback);
+        // }
+        // void RemoveSelf(const any argument, const function<void(NodeBase *const, const any)> callback) { // NOLINT
+        //     NodeBase::RemoveSelf(argument, callback);
+        // }
 
         public:
         Node &operator=(const Node &other) = delete;
         Node &operator=(Node &&other) = delete;
     };
+    class Root final : public Node {
+    };
+    class A final : public Node {
+    };
+    class B final : public Node {
+    };
     class Tree final : public TreeBase {
+
+        public:
+        using TreeBase::Root;
 
         public:
         explicit Tree() = default;
@@ -74,15 +87,18 @@ namespace TreeMachine {
         ~Tree() override = default;
 
         public:
-        void SetRoot(Node *const root, const any argument, const function<void(NodeBase *const, const any)> callback) {
-            TreeBase::SetRoot(root, argument, callback);
-        }
-        void AddRoot(Node *const root, const any argument) {
-            TreeBase::AddRoot(root, argument);
-        }
-        void RemoveRoot(const any argument, const function<void(NodeBase *const, const any)> callback) { // NOLINT
-            TreeBase::RemoveRoot(argument, callback);
-        }
+        using TreeBase::SetRoot;
+        using TreeBase::AddRoot;
+        using TreeBase::RemoveRoot;
+        // void SetRoot(Node *const root, const any argument, const function<void(NodeBase *const, const any)> callback) {
+        //     TreeBase::SetRoot(root, argument, callback);
+        // }
+        // void AddRoot(Node *const root, const any argument) {
+        //     TreeBase::AddRoot(root, argument);
+        // }
+        // void RemoveRoot(const any argument, const function<void(NodeBase *const, const any)> callback) { // NOLINT
+        //     TreeBase::RemoveRoot(argument, callback);
+        // }
 
         public:
         Tree &operator=(const Tree &other) = delete;
@@ -106,8 +122,31 @@ namespace TreeMachine {
 
     TEST(Tests_00, Test_00) { // NOLINT
         auto *const tree = new Tree();
-        tree->AddRoot(new Node(), nullptr);
-        tree->RemoveRoot(nullptr, [](auto *root, [[maybe_unused]] auto arg) { delete root; });
+
+        tree->AddRoot(new Root(), nullptr);
+        EXPECT_NE(tree->Root(), nullptr);
+        EXPECT_EQ(tree->Root()->Tree(), tree);
+        EXPECT_EQ(tree->Root()->TreeRecursive(), tree);
+        EXPECT_EQ(tree->Root()->Parent(), nullptr);
+        EXPECT_EQ(tree->Root()->Activity(), NodeBase::EActivity::Active);
+        EXPECT_EQ(tree->Root()->Children()->size(), 0);
+
+        dynamic_cast<Root *>(tree->Root())->AddChild(new A(), nullptr);
+        dynamic_cast<Root *>(tree->Root())->AddChild(new B(), nullptr);
+        EXPECT_EQ(tree->Root()->Children()->size(), 2);
+        for (const auto *const child : *tree->Root()->Children()) {
+            EXPECT_EQ(child->Tree(), nullptr);
+            EXPECT_EQ(child->TreeRecursive(), tree);
+            EXPECT_EQ(child->Parent(), tree->Root());
+            EXPECT_EQ(child->Activity(), NodeBase::EActivity::Active);
+            EXPECT_EQ(child->Children()->size(), 0);
+        }
+        dynamic_cast<Root *>(tree->Root())->RemoveChildren([]([[maybe_unused]] auto *const child) { return true; }, nullptr, [](auto *const child, [[maybe_unused]] const auto arg) { delete child; });
+        EXPECT_EQ(tree->Root()->Children()->size(), 0);
+
+        tree->RemoveRoot(nullptr, [](auto *const root, [[maybe_unused]] const auto arg) { delete root; }); // NOLINT
+        EXPECT_EQ(tree->Root(), nullptr);
+
         delete tree;
     }
 
